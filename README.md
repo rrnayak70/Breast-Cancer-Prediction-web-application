f90f1c60de417dfbbf65321515254bc73c755790
rrnayak70 <rishabhnayak2002@gmail.com>
2023-05-21 03:43:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Breast-Cancer-Prediction-web-application
+ Well known model predictor of Breast cancer in web application 
---
+++ app.py
@@ -0,0 +1,180 @@
+from flask import Flask, request, session, redirect, url_for, render_template
+from flaskext.mysql import MySQL
+import pymysql 
+import re 
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+from flask import Flask, request, render_template
+
+# Load ML model
+model = pickle.load(open("model.pkl", "rb"))
+
+app = Flask(__name__)
+
+app.secret_key = 'thiddddmyygor'
+
+mysql = MySQL()
+   
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '2003'
+app.config['MYSQL_DATABASE_DB'] = 'flasklogin'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
+
+# http://localhost:5000/pythonlogin/ - this will be the login page
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+ # connect
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+  
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+   
+    # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            #return 'Logged in successfully!'
+            return redirect(url_for('home'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    
+    return render_template('index.html', msg=msg)
+
+# http://localhost:5000/register - this will be the registration page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+ # connect
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+  
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        fullname = request.form['fullname']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+   
+  #Check if account exists using MySQL
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s)', (fullname, username, password, email)) 
+            conn.commit()
+   
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+# http://localhost:5000/home - this will be the home page, only accessible for loggedin users
+@app.route('/login')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+   
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+  
+
+# http://localhost:5000/logout - this will be the logout page
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+# http://localhost:5000/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/profile')
+def profile(): 
+ # Check if account exists using MySQL
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+  
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+  
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
+
+@app.route('/enterpredict', methods=['post'])
+def enterpredict():
+
+     # Put all form entries values in a list
+    features = [float(i) for i in request.form.values()]
+    print(features)
+    # Convert features to array
+    array_features = [np.array(features)]
+    print(array_features)
+
+    # Predict features
+    prediction = model.predict(array_features)
+    output = prediction
+    print(output)
+    # Check the output values and retrive the result with html tag based on the value
+    if output == 0:
+        return render_template("result.html",
+                               Diagnosis_text='The patient is likely to have Benign Tumor!')
+    else:
+        return render_template("result.html",
+                               Diagnosis_text='The patient is likely to have Malign Tumor!')
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
